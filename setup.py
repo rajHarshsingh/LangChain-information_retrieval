b1bbaddcb456c1264c8637f5c7e9edda648d6e21
rajHarshsingh <harshitsingh2015@gmail.com>
2024-07-17 19:16:18 +0530

    install dependencies

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Generative AI Project",
+    version="0.0",
+    author="Harshit",
+    author_email="harshitsingh2015@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
